a51031b51daecddb7f846b9c62c566f05ddddf83
Frank GP <fgp555@gmail.com>
2022-06-19 05:01:55 -0500

    05 Head Dinamic

--- header-footer.js
+++ header-footer.js
@@ -2,25 +2,31 @@ let header = document.querySelector("header");
 let footer = document.querySelector("footer");
 
 header.innerHTML = `
-<a href="/" target="_blank" rel="noopener noreferrer">
-<img class="logo" src="https://i.ibb.co/NjTDVX0/fgp-Web-logo.png" alt="" />
+<a href="/" rel="noopener noreferrer">
+<h2 class="title-nav">Frank GP <span>Web Architect</span></h2>
 </a>
 <nav>
-<a class="menu active" href="/"><i class="fa fa-home"></i></a>
-<a class="menu" href="doc.html">Doc</a>
-<a class="menu" href="lib.html">Lib</a>
 <a class="menu" href="app.html">App</a>
-<a class="menu" href="about.html">About</a>
+<a class="menu" href="lib.html">Lib</a>
+<a class="menu" href="doc.html">Doc</a>
+<a class="menu" href="contact">Contact</a>
 </nav>
 `;
 
 footer.innerHTML = `
-<a href="/" target="_blank" rel="noopener noreferrer">
-<img class="logo" src="https://i.ibb.co/NjTDVX0/fgp-Web-logo.png" alt="" />
+<a href="/" rel="noopener noreferrer">
+<h2 class="title-nav">Frank GP <span>Web Architect</span></h2>
 </a>
 <nav>
-<a class="menu" href="http://instagram.com/fgp555/"><i class="fa fa-instagram"></i> Instagram</a>
-<a class="menu" href="http://fb.com/fgp555/"><i class="fa fa-facebook"></i> Facebook</a>
-<a class="menu" href="http://youtube.com/fgp555/"><i class="fa fa-youtube-play"></i> Youtube</a>
+<a class="menu" target="_blank" href="http://instagram.com/fgp555/"><i class="fa fa-instagram"></i> Instagram</a>
+<a class="menu" target="_blank" href="http://fb.com/fgp555/"><i class="fa fa-facebook"></i> Facebook</a>
+<a class="menu" target="_blank" href="http://youtube.com/fgp555/"><i class="fa fa-youtube-play"></i> Youtube</a>
 </nav>
 `;
+
+// ========== Head import ==========
+
+var ajax = new XMLHttpRequest();
+ajax.open("GET", "template-head.html", false);
+ajax.send();
+document.head.innerHTML = ajax.responseText + document.head.innerHTML;
