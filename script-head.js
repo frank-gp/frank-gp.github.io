26edb4a14da0ef149473205fa577fa74f5800470
Thinkpad <fgp555@gmail.com>
2023-06-25 14:21:34 -0500

    contact update

--- script-head.js
+++ script-head.js
@@ -1,4 +1,4 @@
-document.head.innerHTML += `
+document.head.innerHTML += /* html */`
 <!-- Meta -->
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
