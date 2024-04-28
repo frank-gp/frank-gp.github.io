0d4f5e73d58373e242536a7b61b3ef82dd2ecdd1
Frank GP <fgp555@gmail.com>
2024-04-28 17:42:11 -0500

    add porfolio

---
+++ portfolio/database_temp.js
@@ -0,0 +1,26 @@
+const dataBase = [
+  {
+    id: 0,
+    url: "https://catalogo-web.github.io/catalogo-petulap/",
+    title: "Venta de Laptops",
+    category: "real project",
+    language: [
+      { icon: "icon-html", name: "HTML" },
+      { icon: "icon-css", name: "CSS" },
+      { icon: "icon-js", name: "JS" },
+    ],
+  },
+  {
+    id: 1,
+    url: "https://catalogo-web.github.io/catalogo-petulap/",
+    title: "Venta de Laptops",
+    description: "description web",
+    category: "real project",
+    github: "https://github.com/catalogo-web/catalogo-petulap",
+    language: [
+      { icon: "icon-html", name: "HTML" },
+      { icon: "icon-css", name: "CSS" },
+      { icon: "icon-js", name: "JS" },
+    ],
+  },
+];
