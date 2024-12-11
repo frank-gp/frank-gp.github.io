d7606239ed9957fe9ec2b72207083c5a7ac20511
fgp555 <fgp555@gmail.com>
2024-12-11 15:43:32 -0500

    update portfolio

--- portfolio/database.js
+++ portfolio/database.js
@@ -1,93 +1,94 @@
 const dataBase = [
   {
-    id: 1,
+    id: 241201,
+    url: "https://crefi.giomr.site/admin",
+    image: "https://i.postimg.cc/pXH5G93K/crefi.webp",
+    title: "Appointment System",
+    description: `Creador de citas <br> 
+    <a href='https://www.linkedin.com/in/giovanna-martinez-russo/' target='_blank'>Front: Gio</a> | 
+    <a href='https://www.linkedin.com/in/fgp555/' target='_blank'>Back: Frank</a>
+    `,
+    category: "Real Projects",
+    github: "https://github.com/fgp555/gio-m3-appointments",
+    language: [{ name: "ReactJS" }, { name: "CSS" }, { name: "JavaScript" }, { name: "NestJS" }, { name: "PostgreSQL" }],
+  },
+  {
+    id: 241102,
+    url: "https://youtu.be/_okCMMfx1O4",
+    image: "https://i.postimg.cc/26Kjp5kd/shortener.webp",
+    title: "URL Shortener",
+    description: `Acortador de URL con copias de seguridad y estatisticas`,
+    category: "Real Projects",
+    github: "https://github.com/frank-gp/fgpone",
+    language: [{ name: "HTML" }, { name: "CSS" }, { name: "JavaScript" }, { name: "NodeJS" }, { name: "MongoDB" }],
+  },
+  {
+    id: 241101,
+    url: "https://youtu.be/DY9lwTE08aw",
+    image: "https://i.postimg.cc/pXdQYG7g/shipping.webp",
+    title: "Shipping System",
+    description: `Sistema de envios y entregas con generacion de reportes en PDF`,
+    category: "Real Projects",
+    github: "https://github.com/fgp555/nere-shipping-project",
+    language: [{ name: "HTML" }, { name: "CSS" }, { name: "JavaScript" }, { name: "NestJS" }, { name: "PostgreSQL" }],
+  },
+  {
+    id: 240901,
+    url: "https://youtu.be/vSBttwQO62k",
+    image: "https://i.postimg.cc/3JKBdJYC/bpventures.webp",
+    title: "BP Ventures",
+    description: `Sistema de entregables para la empresa BP Ventures <br>
+    Front: <a href='https://www.linkedin.com/in/giovanna-martinez-russo/' target='_blank'>Gio</a> | 
+    <a href='https://www.linkedin.com/in/vanesa-bucher-623295245/' target='_blank'>Vane</a> | 
+    <a href='https://www.linkedin.com/in/fhdzleon/' target='_blank'>Feli</a> <br>
+    Back: <a href='https://www.linkedin.com/in/fgp555/' target='_blank'>Frank</a> | 
+    <a href='https://www.linkedin.com/in/ricardoolivarit' target='_blank'>Ricardo</a> | 
+    <a href='https://www.linkedin.com/in/josemavsg/' target='_blank'>Jose</a> | 
+    `,
+    category: "Real Projects",
+    github: "https://github.com/fgp555/bpventures.com.ar",
+    language: [{ name: "NextJS" }, { name: "Tailwind" }, { name: "TypeScript" }, { name: "NestJS" }, { name: "PostgreSQL" }],
+  },
+  {
+    id: 103,
+    url: "https://mariaknoll.website/",
+    image: "https://i.postimg.cc/Nfpkq761/mariaknoll-rest-png.webp",
+    title: "mariaknoll.website",
+    description: `Catalogo full stack para vestido de fiesta <br> 
+    <a href='https://www.instagram.com/vanesabucher/' target='_blank'>Front: Vane</a> | 
+    <a href='https://www.linkedin.com/in/fgp555/' target='_blank'>Back: Frank</a>
+    `,
+    category: "Real Projects",
+    github: "https://github.com/buchervanesa/mariaknoll.website",
+    language: [{ name: "HTML" }, { name: "CSS" }, { name: "JavaScript" }, { name: "Node JS" }, { name: "MongoDB" }],
+  },
+  {
+    id: 102,
     url: "https://catalogo-web.github.io/catalogo-petulap/",
+    image: "https://i.postimg.cc/qBcGQdgD/petulap.webp",
     title: "Venta de Laptops",
     description: "Catalogo para la venta de laptop",
     category: "Real Projects",
     github: "https://github.com/catalogo-web/catalogo-petulap",
-    language: [
-      { icon: "icon-html", name: "HTML" },
-      { icon: "icon-css", name: "CSS" },
-      { icon: "icon-js", name: "JS" },
-    ],
+    language: [{ name: "HTML" }, { name: "CSS" }, { name: "JS" }],
   },
   {
-    id: 2,
-    url: "https://mariaknoll.website/",
-    title: "mariaknoll.website",
-    description: "Catalogo full stack para vestido de fiesta",
-    category: "Real Projects",
-    github: "https://github.com/buchervanesa/mariaknoll.website",
-    language: [
-      { icon: "icon-html", name: "HTML" },
-      { icon: "icon-css", name: "CSS" },
-      { icon: "icon-js", name: "JavaScript" },
-      { icon: "icon-nodejs", name: "Node JS" },
-      { icon: "icon-mongodb", name: "MongoDB" },
-    ],
-  },
-  {
-    id: 3,
-    url: "https://webpt19b.web.app/checkpoint",
-    title: "Simulacro Checkpoint",
-    description: "Simulacro para el checkpoin de SoyHenry",
-    category: "Real Projects",
-    language: [
-      { icon: "icon-html", name: "HTML" },
-      { icon: "icon-css", name: "CSS" },
-      { icon: "icon-js", name: "JavaScript" },
-    ],
-  },
-  {
-    id: 4,
+    id: 101,
     url: "https://foudetoi.rest/",
+    image: "https://i.postimg.cc/vHQtNSVY/foudetoi-rest.webp",
     title: "Fou De Toi Restaurant",
     description: "Restaurante vegetariano",
     category: "Real Projects",
-    language: [
-      { icon: "icon-html", name: "HTML" },
-      { icon: "icon-css", name: "CSS" },
-      { icon: "icon-js", name: "JavaScript" },
-    ],
+    language: [{ name: "HTML" }, { name: "CSS" }, { name: "JavaScript" }],
   },
   {
-    id: 5,
-    url: "https://enyeinnovation.github.io/illustrator/",
-    title: "illustrator",
-    description: "landing Pages Responsive con HTML y CSS",
-    category: "Real Projects",
-    github: "https://github.com/enyeinnovation/illustrator",
-    language: [
-      { icon: "icon-html", name: "HTML" },
-      { icon: "icon-css", name: "CSS" },
-      { icon: "icon-js", name: "JavaScript" },
-    ],
-  },
-  {
-    id: 6,
-    url: "https://frankgp.com/app/color/",
-    title: "Color Hex",
-    description: "App Generator Color Hex",
-    category: "Tools",
-    github: "https://frankgp.com/app/color/",
-    language: [
-      { icon: "icon-html", name: "HTML" },
-      { icon: "icon-css", name: "CSS" },
-      { icon: "icon-js", name: "JavaScript" },
-    ],
-  },
-  {
-    id: 7,
+    id: 201,
     url: "https://frankgp.com/app/mini-postman/",
+    image: "https://i.postimg.cc/mkSNr70W/mini-postman.webp",
     title: "Mini-Postman App",
     description: "App Mini-Postman for Get Post and Put Request",
     category: "Tools",
     github: "https://frankgp.com/app/mini-postman/",
-    language: [
-      { icon: "icon-html", name: "HTML" },
-      { icon: "icon-css", name: "CSS" },
-      { icon: "icon-js", name: "JavaScript" },
-    ],
+    language: [{ name: "HTML" }, { name: "CSS" }, { name: "JavaScript" }],
   },
 ];
