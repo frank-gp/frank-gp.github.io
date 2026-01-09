9dbbbfbeb92a180646167772a865a062a3a09890
Frank GP <fgp555@gmail.com>
2026-01-09 09:03:08 -0500

    2026-01-09

--- component/stat.js
+++ component/stat.js
@@ -32,7 +32,7 @@ function statFn() {
         org,
       } = ipData;
 
-      fetch("https://frankgp.com/api/stat/track", {
+      fetch("https://app.frankgp.com/api/stat/track", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
